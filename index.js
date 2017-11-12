b207b8f9b8a08b6a15e54a7becd58f634899488e
Osbaldo Martinez Jr <martinez.osbaldo@gmail.com>
2017-11-12 20:35:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,21 @@ class BoardMember{
     this.training = z;
   }
 }
+BoardMember.prototype.approve = function(){
+  return "You can do that!"
+}
+BoardMember.prototype.doCharity = function(){
+  return "I like to help people."
+}
+BoardMember.prototype.releasePressStatement = function(){
+  return 'No, I must disagree'
+}
+BoardMember.prototype.sayHi = function(){
+  return "You will see great things from Scuber."
+}
+BoardMember.prototype.veto = function(){
+  return 'No, I must disagree'
+}
 BoardMember.prototype.veto = function(){
   return 'No, I must disagree'
 }
